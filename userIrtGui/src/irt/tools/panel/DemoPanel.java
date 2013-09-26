8a2b67778480ccbb57f5391bac3901c46e4922aa
OlexDragon <OlexDragon@eeacd6ae-e507-4818-b1d3-31b3d8136106>
2013-09-26 15:23:39 +0000

    work on translation

---
+++ userIrtGui/src/irt/tools/panel/DemoPanel.java
@@ -0,0 +1,304 @@
+package irt.tools.panel;
+
+import irt.data.Listeners;
+import irt.data.packet.LinkHeader;
+import irt.data.value.Value;
+import irt.data.value.ValueDouble;
+import irt.irt_gui.IrtGui;
+import irt.tools.button.ImageButton;
+import irt.tools.label.ImageLabel;
+import irt.tools.label.LED;
+import irt.tools.panel.head.Panel;
+import irt.tools.panel.subpanel.InfoPanel;
+import resources.Translation;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
+@SuppressWarnings("serial")
+public class DemoPanel extends Panel {
+	private static final float _14 = 14;
+	private static final int ledLockWidth = 81;
+	private static final float _18 = 18;
+	private static final float MUTE_LABEL_FONT_SIZE = 16;
+	private static final Font FONT = new Font("Tahoma", Font.BOLD, (int)MUTE_LABEL_FONT_SIZE);
+	private JTextField textField;
+	private JTextField textField_1;
+	private JSlider slider;
+	private JLabel lblMute;
+	private ImageButton btnMute;
+	private LED ledMute;
+	private LED ledLock;
+	private JLabel lblInputPower;
+	private JLabel lblOutputPower;
+	private JLabel lblTemperature;
+
+	public DemoPanel() {
+		super("THE UNIT IS NOT CONNECTED", 0, 0, 0, 0, 0);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Monitor", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 14), Color.WHITE));
+		panel.setOpaque(false);
+		panel.setBounds(10, 12, 214, 210);
+		panel.setLayout(null);
+		userPanel.add(panel);
+
+		String muteText = Translation.getValue(String.class, "mute", "MUTE");
+		Font font = replaceFont("resource.font", "mute_label_font_size", FONT, _18);
+		
+		ledLock = new LED(Color.GREEN, Translation.getValue(String.class, "lock", "LOCK"));
+		ledLock.setName("Lock");
+		ledLock.setForeground(Color.GREEN);
+		ledLock.setFont(font);
+		ledLock.setBounds(19, 152, Translation.getValue(Integer.class, "monitor.led.lock.size", ledLockWidth), 28);
+		panel.add(ledLock);
+
+		ledMute = new LED(Color.YELLOW, muteText);
+		ledMute.setName("Mute");
+		ledMute.setForeground(Color.GREEN);
+		ledMute.setFont(font);
+		ledMute.setBounds(117, 152, 84, 28);
+		panel.add(ledMute);
+
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 233, 214, 150);
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Control", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 14), Color.WHITE));
+		panel_1.setOpaque(false);
+		panel_1.setBounds(10, 225, 214, 180);
+		panel_1.setLayout(null);
+		userPanel.add(panel_1);
+		
+		btnMute = new ImageButton(new ImageIcon(IrtGui.class.getResource("/irt/irt_gui/images/power-red.png")).getImage());
+		btnMute.setToolTipText(muteText);
+		btnMute.setShadowShiftY(4);
+		btnMute.setShadowShiftX(4);
+		btnMute.setShadowPressedShiftY(1);
+		btnMute.setShadowPressedShiftX(1);
+		btnMute.setName("Button Mute");
+		btnMute.setBounds(13, 104, 33, 33);
+		panel_1.add(btnMute);
+		
+		ImageButton imageButton_1 = new ImageButton(new ImageIcon(IrtGui.class.getResource("/irt/irt_gui/images/whitehouse_button.png")).getImage());
+		imageButton_1.setToolTipText("Store Config");
+		imageButton_1.setShadowShiftY(4);
+		imageButton_1.setShadowShiftX(4);
+		imageButton_1.setShadowPressedShiftY(1);
+		imageButton_1.setShadowPressedShiftX(1);
+		imageButton_1.setName("Store");
+		imageButton_1.setBounds(162, 104, 33, 33);
+		panel_1.add(imageButton_1);
+
+		font = font.deriveFont(Translation.getValue(Float.class, "monitor.leds.font.size", MUTE_LABEL_FONT_SIZE));
+		lblMute = new JLabel(muteText);
+		lblMute.setName("Label Mute");
+		lblMute.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMute.setForeground(Color.YELLOW);
+		lblMute.setFont(font);
+		lblMute.setBounds(46, 110, 84, 20);
+		panel_1.add(lblMute);
+		
+		textField = new JTextField();
+		textField.setEnabled(false);
+		textField.setName("Text Gain");
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setForeground(Color.YELLOW);
+		textField.setFont(FONT);
+		textField.setColumns(10);
+		textField.setCaretColor(Color.WHITE);
+		textField.setBackground(new Color(11, 23, 59));
+		textField.setBounds(13, 45, 188, 20);
+		panel_1.add(textField);
+		
+		JComboBox<String> comboBox = new JComboBox<>();
+		comboBox.addPopupMenuListener(Listeners.popupMenuListener);
+		comboBox.setForeground(Color.YELLOW);
+		comboBox.setBackground(new Color(11, 23, 59));
+		comboBox.setBounds(13, 22, 85, 20);
+		comboBox.addItem("ATTENUATION");
+		((JLabel)comboBox.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+		comboBox.setUI(new BasicComboBoxUI(){
+
+			@Override
+			protected JButton createArrowButton() {
+				return new JButton(){
+
+					@Override
+					public int getWidth() {
+						return 0;
+					}};
+			}
+		});
+		panel_1.add(comboBox);
+		
+		JCheckBox checkBox = new JCheckBox("Step:");
+		checkBox.setEnabled(false);
+		checkBox.setOpaque(false);
+		checkBox.setForeground(Color.WHITE);
+		checkBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		checkBox.setBounds(13, 68, 55, 23);
+		panel_1.add(checkBox);
+		
+		textField_1 = new JTextField();
+		textField_1.setEnabled(false);
+		textField_1.setText("1");
+		textField_1.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_1.setForeground(Color.WHITE);
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		textField_1.setColumns(10);
+		textField_1.setCaretColor(Color.YELLOW);
+		textField_1.setBackground(new Color(11, 23, 59));
+		textField_1.setBounds(74, 68, 127, 20);
+		panel_1.add(textField_1);
+		
+		JComboBox<String> cbLoSelect = new JComboBox<String>();
+		cbLoSelect.setName("LO Select");
+		cbLoSelect.setUI(new BasicComboBoxUI(){
+
+			@Override
+			protected JButton createArrowButton() {
+				return new JButton(){
+
+					@Override
+					public int getWidth() {
+						return 0;
+					}};
+			}
+		});
+		cbLoSelect.setForeground(Color.YELLOW);
+		cbLoSelect.addPopupMenuListener(Listeners.popupMenuListener);
+		cbLoSelect.setBackground(new Color(0x0B,0x17,0x3B));
+		cbLoSelect.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		cbLoSelect.setFont(FONT);
+		cbLoSelect.setBounds(10, 141, 194, 26);
+		cbLoSelect.addItem("LO:1 12800 MHz");
+		cbLoSelect.addItem("LO:2 13050 MHz");
+		panel_1.add(cbLoSelect);
+		
+		slider = new JSlider();
+		slider.setEnabled(false);
+		slider.setMinimum(0);
+		slider.setMaximum(20);
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				Value v = new ValueDouble(0, 0, 20, 1);
+				v.setPrefix(" dB");
+				if (slider.isFocusOwner())
+					v.setValue(slider.getValue());
+					textField.setText(v.toString());
+			}
+		});
+		slider.setOpaque(false);
+		slider.setBounds(234, 12, 28, 397);
+		userPanel.add(slider);
+		slider.setOrientation(SwingConstants.VERTICAL);
+		
+		InfoPanel infoPanel = new InfoPanel((LinkHeader) null);
+		infoPanel.setBounds(10, 11, 286, 104);
+		extraPanel.add(infoPanel);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setOpaque(false);
+		tabbedPane.setBounds(10, 123, 286, 296);
+		extraPanel.add(tabbedPane);
+		((JLabel)cbLoSelect.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+
+		JLabel lblNewLabel = new ImageLabel(new ImageIcon(UserPicobucPanel.class.getResource("/irt/irt_gui/images/logo.gif")),"");
+		tabbedPane.addTab("IRT", null, lblNewLabel, null);
+		
+		font = font.deriveFont(Translation.getValue(Float.class, "monitor.led.ip.fomt.size", _14)).deriveFont(Font.PLAIN);
+
+		lblInputPower = new JLabel(Translation.getValue(String.class, "input_power", "Input Power")+":");
+		lblInputPower.setName("");
+		lblInputPower.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblInputPower.setForeground(new Color(153, 255, 255));
+		lblInputPower.setFont(font);
+		lblInputPower.setBounds(5, 36, 100, 17);
+		panel.add(lblInputPower);
+		
+		JLabel lblDbm = new JLabel("-25 dBm");
+		lblDbm.setName("Input Power");
+		lblDbm.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblDbm.setForeground(Color.WHITE);
+		lblDbm.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDbm.setBounds(118, 36, 82, 17);
+		panel.add(lblDbm);
+		
+		lblOutputPower = new JLabel(Translation.getValue(String.class, "output_power", "Output Power")+":");
+		lblOutputPower.setName("");
+		lblOutputPower.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblOutputPower.setForeground(new Color(153, 255, 255));
+		lblOutputPower.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblOutputPower.setBounds(5, 64, 100, 17);
+		panel.add(lblOutputPower);
+		
+		JLabel lblDbm_1 = new JLabel("40 dBm");
+		lblDbm_1.setName("Output Power");
+		lblDbm_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblDbm_1.setForeground(Color.WHITE);
+		lblDbm_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDbm_1.setBounds(107, 64, 93, 17);
+		panel.add(lblDbm_1);
+		
+		lblTemperature = new JLabel(Translation.getValue(String.class, "temperature", "Temperature")+":");
+		lblTemperature.setName("");
+		lblTemperature.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTemperature.setForeground(new Color(153, 255, 255));
+		lblTemperature.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblTemperature.setBounds(5, 92, 100, 17);
+		panel.add(lblTemperature);
+		
+		JLabel lblC = new JLabel("46 C");
+		lblC.setName("Temperature");
+		lblC.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblC.setForeground(Color.WHITE);
+		lblC.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblC.setBounds(116, 92, 84, 17);
+		panel.add(lblC);
+	}
+
+	@Override
+	public void refresh() {
+		super.refresh();
+
+		Font font = replaceFont("resource.font", "mute_label_font_size", FONT, MUTE_LABEL_FONT_SIZE);
+		lblMute.setFont(font);
+		String text = Translation.getValue(String.class, "mute", "MUTE");
+		lblMute.setText(text);
+		btnMute.setToolTipText(text);
+
+		font = font.deriveFont(Translation.getValue(Float.class, "monitor.leds.font.size", _18));
+		ledMute.setFont(font);
+		ledMute.setText(text);
+		ledLock.setFont(font);
+		ledLock.setSize(Translation.getValue(Integer.class, "monitor.led.lock.size", ledLockWidth), ledLock.getHeight() );
+		ledLock.setText(Translation.getValue(String.class, "lock", "LOCK"));
+
+		font = font.deriveFont(Translation.getValue(Float.class, "monitor.led.ip.fomt.size", _14)).deriveFont(Font.PLAIN);
+		lblInputPower.setFont(font);
+		lblInputPower.setText(Translation.getValue(String.class, "input_power", "Input Power")+":");
+
+		lblOutputPower.setFont(font);
+		lblOutputPower.setText(Translation.getValue(String.class, "output_power", "Output Power")+":");
+
+		lblTemperature.setFont(font);
+		lblTemperature.setText(Translation.getValue(String.class, "temperature", "Temperature")+":");
+	}
+}
---
+++ userIrtGui/src/irt/tools/panel/subpanel/ControlPanelDemo.java
@@ -0,0 +1,151 @@
+package irt.tools.panel.subpanel;
+
+import irt.controller.control.ControllerAbstract;
+import irt.data.IdValue;
+import irt.data.Listeners;
+import irt.irt_gui.IrtGui;
+import irt.tools.button.ImageButton;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
+@SuppressWarnings("serial")
+public class ControlPanelDemo extends JPanel {
+
+	public static final short ATTENUATION= 0;
+	public static final short GAIN 		= 1;
+	public static final short FREQUENCY 	= 2;
+
+	protected JTextField txtGain;
+	protected JSlider slider;
+	private JTextField txtStep;
+	private JCheckBox chckbxStep;
+
+	private ControllerAbstract ñontroller;
+	protected Cursor cursor;
+	protected Color color;
+
+	public ControlPanelDemo() {
+		setLayout(null);
+
+		color = new Color(0x0B,0x17,0x3B);
+		cursor = new Cursor(Cursor.HAND_CURSOR);
+
+		ImageButton btnMute = new ImageButton(new ImageIcon(IrtGui.class.getResource("/irt/irt_gui/images/power-red.png")).getImage());
+		btnMute.setToolTipText("MUTE");
+		btnMute.setName("Button Mute");
+		btnMute.setBounds(50, 13, 33, 9);
+		btnMute.setShadowShiftX(4);
+		btnMute.setShadowShiftY(4);
+		btnMute.setShadowPressedShiftX(1);
+		btnMute.setShadowPressedShiftY(1);
+		btnMute.setCursor(cursor);
+		add(btnMute);
+
+		ImageButton imageButton = new ImageButton(new ImageIcon(IrtGui.class.getResource("/irt/irt_gui/images/whitehouse_button.png")).getImage());
+		imageButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		imageButton.setToolTipText("Store Config");
+		imageButton.setShadowShiftY(4);
+		imageButton.setShadowShiftX(4);
+		imageButton.setShadowPressedShiftY(1);
+		imageButton.setShadowPressedShiftX(1);
+		imageButton.setName("Store");
+		imageButton.setBounds(88, 13, 33, 9);
+		add(imageButton);
+
+		JLabel lblMute = new JLabel("MUTE");
+		lblMute.setName("Label Mute");
+		lblMute.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMute.setForeground(Color.YELLOW);
+		lblMute.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblMute.setBounds(126, 8, 47, 20);
+		add(lblMute);
+		txtGain = new JTextField();
+		txtGain.setForeground(Color.YELLOW);
+		txtGain.setBackground(color);
+		txtGain.setHorizontalAlignment(SwingConstants.CENTER);
+		txtGain.setName("Text Gain");
+		txtGain.setFont(new Font("Tahoma", Font.BOLD, 16));
+		txtGain.setBounds(178, 5 , 156, 26);
+		txtGain.setCaretColor(Color.WHITE);
+		add(txtGain);
+		txtGain.setColumns(10);
+		
+		JComboBox<IdValue> comboBox = new JComboBox<>();
+		comboBox.setUI(new BasicComboBoxUI());//remove border
+		comboBox.addPopupMenuListener(Listeners.popupMenuListener);
+
+		comboBox.setBounds(339, 17, 1, 1);
+		comboBox.setUI(new BasicComboBoxUI(){
+
+			@Override
+			protected JButton createArrowButton() {
+				return new JButton(){
+
+					@Override
+					public int getWidth() {
+						return 0;
+					}};
+			}
+		});
+		comboBox.setBackground(color);
+		comboBox.setForeground(Color.YELLOW);
+		comboBox.setCursor(cursor);
+		add(comboBox);
+		
+		chckbxStep = new JCheckBox("Step:");
+		chckbxStep.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				slider.setSnapToTicks(chckbxStep.isSelected());
+				txtGain.requestFocusInWindow();
+			}
+		});
+		chckbxStep.setForeground(Color.WHITE);
+		chckbxStep.setOpaque(false);
+		chckbxStep.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		chckbxStep.setBounds(345, 6, 55, 23);
+		add(chckbxStep);
+		
+		txtStep = new JTextField();
+ 		txtStep.setBackground(color);
+		txtStep.setForeground(Color.WHITE);
+		txtStep.setText("1");
+		txtStep.setHorizontalAlignment(SwingConstants.CENTER);
+		txtStep.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtStep.setColumns(10);
+		txtStep.setBounds(152, 36, 146, 26);
+		txtStep.setCaretColor(Color.YELLOW);
+		add(txtStep);
+		
+		slider = new JSlider();
+
+		addAncestorListener(new AncestorListener() {
+			public void ancestorAdded(AncestorEvent event) {
+			}
+
+			public void ancestorMoved(AncestorEvent event) {}
+
+			public void ancestorRemoved(AncestorEvent event) {
+				if(ñontroller!=null)
+					ñontroller.setRun(false);
+				ñontroller = null;
+			}
+		});
+	}
+}
