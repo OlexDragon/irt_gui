413b49ba27688b6ac404a90443abbd1f83ae3a3d
OlexDragon <OlexDragon@eeacd6ae-e507-4818-b1d3-31b3d8136106>
2017-01-18 18:38:09 +0000

    Created Signal Generator

---
+++ projects/fx.control/src/main/java/irt/fx/control/generator/SignalGeneratorFx.java
@@ -0,0 +1,226 @@
+package irt.fx.control.generator;
+
+import java.io.IOException;
+import java.util.Observer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.prefs.Preferences;
+
+import javax.activation.UnsupportedDataTypeException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import irt.data.IrtGuiProperties;
+import irt.data.tools.ToolsFrequency;
+import irt.data.tools.enums.FrequencyUnits;
+import irt.data.tools.enums.PowerUnits;
+import irt.data.tools.enums.ToolsPower;
+import irt.data.tools.enums.ToolsState;
+import irt.fx.control.prologix.PrologixFx;
+import irt.packet.ToolsFrequencyPacket;
+import irt.packet.ToolsIdPacket;
+import irt.packet.ToolsOutputPacket;
+import irt.packet.ToolsPacket;
+import irt.packet.ToolsPowerPacket;
+import irt.packet.interfaces.PacketToSend;
+import irt.serial.port.enums.SerialPortStatus;
+import irt.services.listeners.NumericChecker;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class SignalGeneratorFx extends AnchorPane{
+
+	private Logger logger = LogManager.getLogger();
+	private static final Preferences prefs = Preferences.userRoot().node(IrtGuiProperties.PREFS_NAME);
+
+    @FXML private GridPane gridPane;
+    @FXML private TextField textFieldAddress;
+    @FXML private Label labelId;
+    @FXML private TextField textFieldFrequency;
+    @FXML private TextField textFieldPower;
+    @FXML private ComboBox<ToolsState> comboBoxOutput;
+
+    private PrologixFx prologix;
+	private final ToolsPacket idPacket	= new ToolsIdPacket();
+	private final ToolsPacket frPacket	= new ToolsFrequencyPacket();
+	private final ToolsPacket pwPacket	= new ToolsPowerPacket();
+	private final ToolsPacket outPacket	= new ToolsOutputPacket();
+	private EventHandler<ActionEvent> onAction;
+
+	public SignalGeneratorFx() {
+
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/signal_generator.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+    	final Observer idObserver = (o, arg)->{
+
+    		final byte[] answer = ((PacketToSend)o).getAnswer();
+    		Platform.runLater(()->labelId.setText(answer!=null ? new String(answer) : "N/A"));
+    	};
+		idPacket.addObserver(idObserver);
+	}
+
+    @FXML private void initialize() {
+		new NumericChecker(textFieldAddress.textProperty());
+		final String addr = prefs.get("sgAddr", "19");
+		textFieldAddress.setText(addr);
+		textFieldAddress.focusedProperty().addListener((observable, oldValue, newValue)->{
+			if(!newValue)
+				prefs.put("powerMeterAddr", textFieldAddress.getText());
+		});
+
+		comboBoxOutput.getItems().addAll(ToolsState.values());
+		onAction = comboBoxOutput.getOnAction();
+	}
+
+    @FXML void onGetAll() {
+		prologix.send(textFieldAddress.getText(), idPacket);
+		onGetFrequency();
+		onGetPower();
+		onGetRF();
+    }
+
+	@FXML void onGetFrequency() {
+		logger.traceEntry();
+		frPacket.addObserver((o,arg)->Platform.runLater(()->{
+
+			frPacket.deleteObservers();
+			final byte[] answer = ((PacketToSend)o).getAnswer();
+			setText(textFieldFrequency, answer, ()->FrequencyUnits.toString(answer));
+
+		}));
+		prologix.send(textFieldAddress.getText(), frPacket);
+	}
+
+	private void setText(TextField textField, byte[] answer, Supplier<String> supplier) {
+
+		final ObservableList<String> styleClass = textField.getStyleClass();
+
+		if(answer!=null){
+			final String text = supplier.get();
+			textField.setText(text);
+			styleClass.remove("error");
+
+		}else if(!styleClass.contains("error"))
+				styleClass.add("error");
+	}
+
+    @FXML void onSetFrequency() {
+    	final String text = textFieldFrequency.getText();
+
+    	if(text.isEmpty() || !text.matches(".*\\d+.*")){
+    		onGetFrequency();
+    		return;
+    	}
+
+		try {
+			frPacket.getCommand().setValue(new ToolsFrequency(text));
+		} catch (UnsupportedDataTypeException e) {
+			logger.catching(e);
+		}
+		frPacket.deleteObservers();
+		prologix.send(textFieldAddress.getText(), frPacket);
+    }
+
+    @FXML void onGetPower() {
+		pwPacket.addObserver((o,arg)->{
+
+			pwPacket.deleteObservers();
+			final byte[] answer = ((PacketToSend)o).getAnswer();
+			setText(textFieldPower, answer, ()->PowerUnits.toString(answer));
+
+		});
+		prologix.send(textFieldAddress.getText(), pwPacket);
+    }
+
+    @FXML void onSetPower() {
+    	final String text = textFieldPower.getText();
+
+    	if(text.isEmpty() || !text.matches(".*\\d+.*")){
+    		onGetPower();
+    		return;
+    	}
+
+		try {
+			pwPacket.getCommand().setValue(new ToolsPower(text));
+		} catch (UnsupportedDataTypeException e) {
+			logger.catching(e);
+		}
+		pwPacket.deleteObservers();
+		prologix.send(textFieldAddress.getText(), pwPacket);
+    }
+
+    @FXML void onGetRF() {
+		outPacket.addObserver((o,arg)->{
+
+			final ObservableList<String> styleClass = comboBoxOutput.getStyleClass();
+			outPacket.deleteObservers();
+			final byte[] answer = ((PacketToSend)o).getAnswer();
+
+			Platform.runLater(()->{
+				comboBoxOutput.setOnAction(null);
+
+				if(answer!=null){
+					final ToolsState valueOf = ToolsState.valueOf(answer);
+					comboBoxOutput.getSelectionModel().select(valueOf);
+					styleClass.remove("error");
+
+				}else if(!styleClass.contains("error")){
+					comboBoxOutput.getSelectionModel().select(-1);
+					styleClass.add("error");
+				}
+
+				comboBoxOutput.setOnAction(onAction);
+			});
+		});
+		prologix.send(textFieldAddress.getText(), outPacket);
+    }
+
+    @FXML void onSetRF(){
+		logger.traceEntry();
+
+    	final int selectedIndex = comboBoxOutput.getSelectionModel().getSelectedIndex();
+
+    	if(selectedIndex<0){
+    		onGetRF();
+    		return;
+    	}
+
+    	try {
+			outPacket.getCommand().setValue(comboBoxOutput.getSelectionModel().getSelectedItem());
+			prologix.send(textFieldAddress.getText(), outPacket);
+		} catch (UnsupportedDataTypeException e) {
+			logger.catching(e);
+		}
+    }
+
+    public void setPrologix(PrologixFx prologix){
+    	this.prologix = prologix;
+
+    	final Consumer<Boolean> consumer = comPrtNotOpend-> gridPane.getChildren().parallelStream().filter(Button.class::isInstance).map(Button.class::cast).forEach(b->b.setDisable(comPrtNotOpend));
+
+    	//Status change action
+    	prologix.addStatusChangeAction(consumer);
+
+    	//set current status
+    	consumer.accept(prologix.getSerialPortStatus() != SerialPortStatus.OPEND);
+    }
+}
