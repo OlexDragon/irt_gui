f92a9d9fb81625a14f6b723b913a390b861e7b45
OlexDragon <OlexDragon@eeacd6ae-e507-4818-b1d3-31b3d8136106>
2017-05-04 13:18:48 +0000

    fixed: log4j Error writing to stream

--- j_irt_gui_3/src/irt/irt_gui/IrtGui.java
+++ j_irt_gui_3/src/irt/irt_gui/IrtGui.java
@@ -73,7 +73,7 @@ public class IrtGui extends IrtMainFrame {
 	private static LoggerContext ctx = DumpControllers.setSysSerialNumber(null);//need for log file name setting
 	private static final Logger logger = (Logger) LogManager.getLogger();
 
-	public static final String VERTION = "- 3.114";
+	public static final String VERTION = "- 3.116";
 	private boolean connected;
 
 	protected HeadPanel headPanel;
