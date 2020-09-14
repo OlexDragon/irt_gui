0b786516ad8b883bf4034c3e3a16f6e3e6e19122
Oleksandr Potomkin <potomkin@hotmail.com>
2020-09-14 10:50:17 -0400

    Serial port listener added to commit.

---
+++ j_irt_gui_3/src/irt/controller/serial_port/SerialPortListener.java
@@ -0,0 +1,114 @@
+package irt.controller.serial_port;
+
+import java.util.Arrays;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+import irt.data.ThreadWorker;
+import jssc.SerialPortEventListener;
+
+public class SerialPortListener implements SerialPortDataListener, SerialPortEventListener, purejavacomm.SerialPortEventListener {
+	private final static Logger logger = LogManager.getLogger();
+
+	private final Runnable action;
+	private byte[] buffer;
+
+	public SerialPortListener(SerialPortInterface serialPort) {
+
+		action = 
+				()->{
+
+					try {
+
+						synchronized (SerialPortListener.this) {
+
+							final int bytesAvailable = serialPort.bytesAvailable();
+							logger.debug("bytesAvailable: {}", bytesAvailable);
+
+							if(bytesAvailable<=0)
+								return;
+
+
+							byte[] b = null;
+							try { b = serialPort.readBytes(bytesAvailable); } catch (Exception e) { logger.catching(e); }
+
+							logger.debug("\nread: {};\nbuffer: {};", b, buffer);
+
+							if(buffer==null)
+								buffer = b;
+
+							else {
+								final byte[] copyOfBuffer = Arrays.copyOf(buffer, buffer.length + b.length);
+								System.arraycopy(b, 0, copyOfBuffer, buffer.length, b.length);
+								buffer = copyOfBuffer;
+							}
+						}
+
+					} catch (Exception e) {
+						logger.catching(e);
+					}
+
+					synchronized(serialPort){
+						serialPort.notify();
+					}
+
+				};
+	}
+
+	@Override public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+
+	@Override
+	public void serialEvent(SerialPortEvent event) {
+		logger.traceEntry();
+		final int eventType = event.getEventType();
+
+		if (eventType != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+			return;
+
+		ThreadWorker.runThread(action, "jSerialComm Serial Port Listener");
+	}
+
+	public byte[] getBuffer() {
+		return buffer;
+	}
+
+	public void clear() {
+		buffer = null;
+	}
+
+	public synchronized byte[] getBytes(int size) {
+
+		byte[] result = Arrays.copyOf(buffer, size);
+		buffer = Arrays.copyOfRange(buffer, size, buffer.length);
+
+		return result;
+	}
+
+	@Override
+	public void serialEvent(jssc.SerialPortEvent event) {
+		logger.traceEntry();
+		final int eventType = event.getEventType();
+
+		if (eventType != jssc.SerialPortEvent.RXCHAR)
+			return;
+
+		ThreadWorker.runThread(action, "JSSC Serial Port Listener");
+	}
+
+	@Override
+	public void serialEvent(purejavacomm.SerialPortEvent event) {
+		logger.traceEntry();
+		final int eventType = event.getEventType();
+		logger.error("eventType:{}", eventType);
+
+		if (eventType != jssc.SerialPortEvent.RXCHAR)
+			return;
+
+		ThreadWorker.runThread(action, "Purejavacomm Serial Port Listener");
+	}
+}
